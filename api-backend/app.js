b124a78380e889b8873ccdcc82172ac7fa28158e
Razielini <razielco@gmail.com>
2020-10-22 17:34:24 -0600

    Get Data From API
    Now it works, added env.example and axios to get the request

--- api-backend/app.js
+++ api-backend/app.js
@@ -3,9 +3,27 @@ const app = express();
 const slash = require('express-slash');
 const cors = require('cors')
 const helmet = require('helmet');
+const axios = require('axios');
+const { API_URL, API_KEY } = require('./config');
 
 app.get('/hello', function (req, res) {
   res.send('[GET] Cat Nations API HELLO');
+});
+
+app.get('/cats-breed', function (req, res) {
+  axios({
+    method: 'get',
+    url: `${API_URL}/v1/breeds`,
+    auth: {
+        'x-api-key': API_KEY
+    }
+  })
+  .then(function (response) {
+    res.send(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+      console.log(error);
+  });
 });
 
 // Settings
@@ -15,7 +33,6 @@ app.use(express.urlencoded({ extended: false }));
 
 // Middlewares
 app.use(helmet());
-
 app.use(slash());
 
 module.exports = app;
