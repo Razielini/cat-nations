1157631cb3f12a3a6ed5c5370b3cea2cac4b1bae
Razielini <razielco@gmail.com>
2020-10-23 15:07:16 -0600

    Error Handlers
    Error handler with Boom

---
+++ api-backend/utils/errorHandlers.js
@@ -0,0 +1,27 @@
+const boom = require('@hapi/boom');
+const response = require('./responses');
+
+function wrapErrors(err, req, res, next) {
+  if (!err.isBoom) next(boom.badImplementation(err));
+  next(err);
+}
+
+function errorHandler(err, req, res, next) { // eslint-disable-line
+  const {
+    output: { statusCode, payload }
+  } = boom.internal();
+  response.error(req, res, payload.message, statusCode);
+}
+
+function notFoundHandler(req, res) {
+  const {
+    output: { statusCode, payload }
+  } = boom.notFound();
+  response.error(req, res, payload.message, statusCode);
+}
+
+module.exports = {
+  notFoundHandler,
+  wrapErrors,
+  errorHandler
+};
