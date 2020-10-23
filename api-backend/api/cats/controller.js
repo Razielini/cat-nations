d0d6dee4cb2841bc2398ffc3fe1954a43959519b
Razielini <razielco@gmail.com>
2020-10-23 13:51:55 -0600

    Update controller.js

--- api-backend/api/cats/controller.js
+++ api-backend/api/cats/controller.js
@@ -4,12 +4,12 @@ module.exports = function (injectedStore) {
   const getAll = async () => {
     const results = await store.getAll();
     return results;
-  }
+  };
 
   const searchCat = async (catsName) => {
     const results = await store.searchCat(catsName);
     return results;
-  }
+  };
 
   return {
     getAll,
