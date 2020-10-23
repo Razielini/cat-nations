995362440e2501935d5521132348806d0ea20812
Razielini <razielco@gmail.com>
2020-10-23 13:43:59 -0600

    Update Files
    Updating with the mocks storage

--- api-backend/api/cats/controller.js
+++ api-backend/api/cats/controller.js
@@ -1,12 +1,14 @@
 module.exports = function (injectedStore) {
   let store = injectedStore;
 
+  if (!store) store = require('../../__mocks__/cats.mocks').Cats;
+
   const getAll = async () => {
     const results = await store.getAll();
     return results;
   }
 
-  const searchCat = async (catsName) => {
+  const searchCat = async catsName => {
     const results = await store.searchCat(catsName);
     return results;
   }
