eb955a0e44fbfbf14d1efbb4bba1d10431503770
Razielini <razielco@gmail.com>
2020-10-25 10:17:25 -0700

    Add the config vars
    Sentry DNS & ID

--- api-backend/config/index.js
+++ api-backend/config/index.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const config = {
   PORT: process.env.PORT,
   API_URL: process.env.API_URL,
-  API_KEY: process.env.API_KEY
+  API_KEY: process.env.API_KEY,
+  SENTRY_DNS: process.env.SENTRY_DNS,
+  SENTRY_ID: process.env.SENTRY_ID
 };
 
 module.exports = config;
